ebcdc0976c2c89aad9cd4dc6c5e7168123de138e
jkga <johnkennethgibasabella@gmail.com>
2020-04-18 23:22:03 +0800

    test: log req

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -10,9 +10,9 @@ export default async(req, res) => {
   let options = {
     lang: req.query.lang ? req.query.lang.toString() : null
   } 
-
+  console.log(req)
+  console.log(req.query)
   let profile = await content.getContentFromGithub(req.query.username, options).catch(e => {})
-  console.log(profile)
   if(!profile) return notFound(res, JSON.stringify({ error: 'profile not found' }))
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')

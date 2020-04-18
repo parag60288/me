84d3df79d68ce26078513fee5e0badc5b26abb16
jkga <johnkennethgibasabella@gmail.com>
2020-04-18 23:27:57 +0800

    test: api route in next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
       return [
         { source: '/:username/:template?', destination: '/user' },
         { source: '/preview/templates/:template', destination: '/preview' },
+        { source: '/api', destination: '/api' },
         { source: '/', destination: '/homepage' },
       ];
     },

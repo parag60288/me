5e935f9ddd07e9989145d42eac907fcb91685b60
jkga <johnkennethgibasabella@gmail.com>
2020-04-18 23:31:50 +0800

    test: move up api

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = {
     serverless: true,
     async rewrites() {
       return [
+        { source: '/api', destination: '/api' },
         { source: '/:username/:template?', destination: '/user' },
         { source: '/preview/templates/:template', destination: '/preview' },
-        { source: '/api', destination: '/api' },
         { source: '/', destination: '/homepage' },
       ];
     },
